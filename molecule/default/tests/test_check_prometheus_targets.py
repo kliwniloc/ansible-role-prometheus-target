4dbd1e660fe815a2cc6032c54a2c1d1dbf62cd7b
Colin Wilk <colin.wilk@tum.de>
2023-05-27 20:00:19 +0200

    Add molecule with some simple test cases
    Signed-off-by: Colin Wilk <colin.wilk@tum.de>

---
+++ molecule/default/tests/test_check_prometheus_targets.py
@@ -0,0 +1,16 @@
+testinfra_hosts = ['prometheus']
+
+
+def test_check_hosts_added(host):
+    t1 = host.file('/opt/target1.yml')
+    t2 = host.file('/opt/target2.yml')
+    t3 = host.file('/opt/target3.yml')
+
+    assert t1.exists
+    assert t2.exists
+    assert t3.exists
+
+    assert '  - application' in t1.content_string
+    assert '  - test1' in t2.content_string
+    assert '  - test2' in t2.content_string
+    assert '  - application_AA' in t3.content_string
